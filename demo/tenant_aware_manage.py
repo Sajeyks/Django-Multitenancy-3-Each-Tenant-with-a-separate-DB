f2afa4b286502f865704d3d26b4d70ab90b4b331
Sajeyks <sajeyksmwangi@gmail.com>
2024-02-23 02:55:57 +0300

    Add tenant-aware management script

---
+++ demo/tenant_aware_manage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+if __name__ == "__main__":                  
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    # new ....
+    from django.db import connection
+    import django
+    django.setup()
+    from main.middleware import set_db_for_router
+    
+    args = sys.argv
+    db = args[1]
+    with connection.cursor() as cursor:
+        set_db_for_router(db)
+        del args[1]
+        execute_from_command_line(args)
